f3a11809a7c614d540238dd11790cc54d2fc11e5
JazminAime <jazminfalcon@hotmail.com>
2024-08-23 17:23:45 -0300

    funcionalidad agregar categorias, trabajando con local storage

--- main.js
+++ main.js
@@ -95,10 +95,6 @@ mostrarReportes.addEventListener('click', function() {
   mostrarSeccion(reportes);
 });
 
-mostrarEditar.addEventListener('click', function(){
-  mostrarSeccion(editCategoria);
-});
-
 mostrarOperacion.addEventListener('click', function(){
   mostrarSeccion(vistaOperacion);
 });
@@ -128,3 +124,86 @@ cancelarEdit.addEventListener('click', function(){
 });
 
 
+// Agregar, editar y eliminar categorias
+const inputAggCategorias = document.getElementById('categoria-nombre');
+const buttonAggCategorias = document.getElementById('btn-agg-categoria');
+const divCategorias =  document.getElementById('container-categorias');
+
+function agregarCategorias () {
+  const nombreCategoria = inputAggCategorias.value.trim();
+
+  if (nombreCategoria === '') {
+    alert ('No puede estar vacio este campo.')
+    return;
+  };
+
+  let categorias = JSON.parse(localStorage.getItem('categorias')) || [];
+
+  if (categorias.includes(nombreCategoria)) {
+    alert('La categoría ya existe.');
+    return;
+  }
+
+  categorias.push(nombreCategoria);
+  localStorage.setItem('categorias', JSON.stringify(categorias));
+  inputAggCategorias.value = '';
+  mostrarCategoria();
+};
+
+function mostrarCategoria () {
+  let categorias = JSON.parse(localStorage.getItem('categorias')) || [];
+
+  divCategorias.innerHTML = '';
+
+    for (let i = 0; i < categorias.length; i++){
+      // Crear contenedor para cada categoria
+      const categoriaContainer = document.createElement('div');
+      categoriaContainer.className = 'flex items-center justify-between p-2 mb-2 bg-purple-100 rounded-lg';
+
+      // Crear elemento para cada nombre de categoria
+      const categoriaElement = document.createElement('p');
+      categoriaElement.className = 'text-purple-600 flex-1';
+      categoriaElement.textContent = categorias[i];
+      categoriaContainer.appendChild(categoriaElement);
+
+      // Crear el contenedor para los botones
+      const buttonContainer = document.createElement('div');
+      buttonContainer.className = 'flex gap-2';
+
+      // Crear boton editar
+      const editButton = document.createElement('a');
+      editButton.className = 'text-purple-600 hover:text-black';
+      editButton.href = '#';
+      editButton.textContent = 'Editar';
+
+      editButton.addEventListener('click', function(){
+        mostrarSeccion(editCategoria);
+        const editarCategoriaInput = document.getElementById('editar-categoria-nombre');
+        editarCategoriaInput.value = categorias[i];
+
+        const confirmarEditar = document.getElementById('confirm-edit');
+        confirmarEditar.addEventListener('click', function(){
+          const nuevaCategoria = editarCategoriaInput.value;
+        })
+      })
+
+      buttonContainer.appendChild(editButton);
+
+      // Crear boton eliminar
+      const deleteButton = document.createElement('a');
+      deleteButton.className = 'text-purple-600 hover:text-black';
+      deleteButton.href = '#';
+      deleteButton.textContent = 'Eliminar';
+      buttonContainer.appendChild(deleteButton);
+
+      // Añadir contenedor de botones al contenedor principal
+      categoriaContainer.appendChild(buttonContainer);
+
+      // Añadir el contenedor al contenedor principal de las categorias
+      divCategorias.appendChild(categoriaContainer)
+  }
+};
+
+buttonAggCategorias.addEventListener('click', agregarCategorias)
+
+window.onload = mostrarCategoria;
